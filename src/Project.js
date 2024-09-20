bd0a594dc65a5fa7dc42440c7e0d8fa8ecd62579
betoniochristian <betoniochristian22@gmail.com>
2024-09-20 12:47:05 +0800

    new commit

--- src/Project.js
+++ src/Project.js
@@ -14,14 +14,34 @@ class Project extends React.Component{
         this.state ={
             currentState: 'All',
         }
+        this.aboutRef = React.createRef();
     }
+    componentDidMount(){
+        const options = {
+            root: null,
+            rootMargin: '0px',
+            threshold: 0.1,
+        };
 
+        const callback = (entries) => {
+            entries.forEach((entry) => {
+                if (entry.isIntersecting) {
+                    entry.target.classList.add('animate');
+                }
+            });
+        };
+
+        const observer = new IntersectionObserver(callback, options);
+        if (this.aboutRef.current) observer.observe(this.aboutRef.current);
+    }
     handleNavChange = (section) => {
         this.setState({ currentState: section });
     }
+
+   
     render(){
         return(
-            <div className="information-container">
+            <div className="information-container" ref={this.aboutRef}>
                 <div className="about">
                     <h1>Portfolio</h1>
                 <hr className="hr-about"/>
